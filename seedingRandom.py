498d5c59f8d9724eb984bd520c32f383371523a3
Jacob Foulkrod <foulkrod@umich.edu>
2019-11-27 22:06:49 -0500

    cool

--- seedingRandom.py
+++ seedingRandom.py
@@ -7,8 +7,13 @@ random.seed(seed)
 random.seed(9328362664)
 print(random.randint(0,1000))
 print(random.randint(0,1000))
+cool = True
+uncool = False
 print(random.randint(0,1000))
 print(random.randint(0,1000))
+print("this is a cooler function than yours")
+print("i mean come on, I'm a pretty cool guy")
+print("but i suppose you're pretty cool too")
 print(random.randint(0,1000))
-print(random.randint(0,1000))
-print(random.randint(0,1000))
+today = "11/27/2019"
+turkey = "i'm sorry for tomorrow"
