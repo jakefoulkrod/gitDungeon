1887a536b90dac81c292bffaf15c3942e9ea3a77
Jacob Foulkrod <foulkrod@umich.edu>
2019-11-27 21:45:25 -0500

    seedingRandom

---
+++ seedingRandom.py
@@ -0,0 +1,12 @@
+import random
+
+seed = random.randint(0, 10000000000)
+print(seed)
+random.seed(seed)
+
+random.seed(9328362664)
+print(random.randint(0,1000))
+print(random.randint(0,1000))
+print(random.randint(0,1000))
+print(random.randint(0,1000))
+print(random.randint(0,1000))
